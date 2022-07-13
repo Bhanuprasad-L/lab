3c6d4f9adfb8562a415860c5b6faf4c9462cf6a1
Bhanuprasad L <96941608+bhrx@users.noreply.github.com>
2022-07-14 01:19:50 +0530

    Update bellmanford.c

--- bellmanford.c
+++ bellmanford.c
@@ -1,53 +1,49 @@
 #include<stdio.h>
-
-
-int Bellman_Ford(int G[20][20] , int V, int E, int edge[20][2])
-{
-    int i,u,v,k,distance[20],parent[20],S,flag=1;
-    for(i=0;i<V;i++)
-        distance[i] = 1000 , parent[i] = -1 ;
-        printf("Enter source: ");
-        scanf("%d",&S);
-        distance[S-1]=0 ;
-    for(i=0;i<V-1;i++)
-    {
-        for(k=0;k<E;k++)
-        {
-            u = edge[k][0] , v = edge[k][1] ;
-            if(distance[u]+G[u][v] < distance[v])
-                distance[v] = distance[u] + G[u][v] , parent[v]=u ;
+#include<stdlib.h>
+int maxi=999;
+void bellman(int g[10][3],int v,int e,int s){
+    int dis[10],i,j;
+    for(i=0;i<v;i++){
+        dis[i]=maxi;
+    }
+    dis[s]=0;
+    for(i=0;i<v-1;i++){
+        for(j=0;j<e;j++){
+            if(dis[g[j][0]]!=maxi && dis[g[j][0]] + g[j][2]<dis[g[j][1]]){
+                dis[g[j][1]]=dis[g[j][0]] + g[j][2];
+            }
         }
     }
-    for(k=0;k<E;k++)
-        {
-            u = edge[k][0] , v = edge[k][1] ;
-            if(distance[u]+G[u][v] < distance[v])
-                flag = 0 ;
+    //checking negative cycle optional
+    for(i=0;i<e;i++){
+        int x=g[i][0];
+        int y=g[i][1];
+        int wt=g[i][2];
+        if(dis[x]!=maxi && dis[x]+wt<dis[y]){
+            printf("graph contain negative cycle\n");
         }
-        if(flag)
-            for(i=0;i<V;i++)
-                printf("Vertex %d -> cost = %d parent = %d\n",i+1,distance[i],parent[i]+1);
+    }
+    printf("Vertex distance    source\n");
+    for(i=0;i<v;i++){
+        printf("%d             %d\n",i,dis[i]);
+    }  
 
-        return flag;
 }
-
-int main()
-{
-    int V,edge[20][2],G[20][20],i,j,k=0;
-    printf("Enter no. of vertices: ");
-    scanf("%d",&V);
-    printf("Enter graph in matrix form:\n");
-    for(i=0;i<V;i++)
-        for(j=0;j<V;j++)
-        {
-            scanf("%d",&G[i][j]);
-            if(G[i][j]!=0)
-                edge[k][0]=i,edge[k++][1]=j;
+int main(){
+    int v,e,i,j,g[10][3],s=0;
+    printf("Enter the number of edges\n");
+    scanf("%d",&e);
+    printf("Enter the number of vertex\n");
+    scanf("%d",&v);
+    printf("Enter the graph(u,v,weight)\n");
+    for(i=0;i<e;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&g[i][j]);
         }
-
-    if(Bellman_Ford(G,V,k,edge))
-        printf("\nNo negative weight cycle\n");
-    else printf("\nNegative weight cycle exists\n");
+    }
+    printf("Enter the source\n");
+    scanf("%d",&s);
+    bellman(g,v,e,s);
     return 0;
 }
 
